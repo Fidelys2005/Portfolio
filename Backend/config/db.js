28cc9be2f7a08a484e5e02278436b5bdef139804
BOTOVELO Fidelys <botovelofidelys@gmail.com>
2026-04-24 23:50:45 +0100

    Fix Mysql connection for production

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,12 +2,14 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    port:3306,
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: false
+    ssl: {
+        rejectUnauthorized:false
+    }
 });
 
 db.connect((err) => {
